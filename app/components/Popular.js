be10adcdc209bc6d64e3c1165a60c505c6c4fb2b
Holmes, Morgan <morganholmes49@gmail.com>
2019-11-12 12:58:26 -0500

    Add state to buttons

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,16 +3,35 @@ import ReactDOM from 'react-dom'
 
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLanguage: "All"
+        }
+
+        this.updateLanguage = this.updateLanguage.bind(this);
+    }
+
+    updateLanguage(l) {
+        this.setState({
+            selectedLanguage: l
+        })
+    }
 
     render() {
-        const langs = ["JavaScript","R","Java", "CSS","Python"];
+        console.log(this.state.selectedLanguage)
+        const langs = ["All","JavaScript","R","Java", "CSS","Python"];
         return (
             <ul className="flex-center">
                 {langs.map((l,i)=> (
                     <li key={l}>
-                        <button className="btn-clear nav-link">
+                        <button 
+                            className="btn-clear nav-link"
+                            style={l=== this.state.selectedLanguage? {color: 'rgb(187, 46,31)'}: null}
+                            onClick={()=> this.updateLanguage(l)}>
                             {l}
-                            </button>
+                        </button>
                     </li>
                 ))
                 }
