ba642f617153622e9007c40c17365ac795567121
Holmes, Morgan <mholmes@microstrategy.com>
2019-12-03 12:50:55 -0500

    extract tooltip to a class for reuse

--- app/components/Results.js
+++ app/components/Results.js
@@ -4,29 +4,10 @@ import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from '
 import Card from "./Card"
 import PropTypes from "prop-types"
 import Loading from "./Loading"
+import Tooltip from "./Tooltip.js"
 
 //styles for the tooltip 
-const styles = {
-    container: {
-      position: 'relative',
-      display: 'flex'
-    },
-    tooltip: {
-      boxSizing: 'border-box',
-      position: 'absolute',
-      width: '160px',
-      bottom: '100%',
-      left: '50%',
-      marginLeft: '-80px',
-      borderRadius: '3px',
-      backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
-      padding: '7px',
-      marginBottom: '5px',
-      color: '#fff',
-      textAlign: 'center',
-      fontSize: '14px',
-    }
-  }
+
 class ProfileList extends React.Component {
     constructor(props) {
         super(props);
@@ -58,23 +39,19 @@ class ProfileList extends React.Component {
                     {profile.name}
                 </li>
                 {profile.location && (
-                    <li onMouseOver={()=> this.mouseOver("hoveringLocation")}
-                        onMouseOut={()=> this.mouseOut("hoveringLocation")}
-                        style={styles.container}
-                    >
-                        {hoveringLocation === true && <div style={styles.tooltip}>User's Location</div>}
-                        <FaCompass color='rgb(144, 115, 255)' size={22} />
-                        {profile.location}
+                    <li>
+                        <Tooltip text="User's Location">
+                            <FaCompass color='rgb(144, 115, 255)' size={22} />
+                            {profile.location}
+                        </Tooltip>
                     </li>
                 )}
                 {profile.company && (
-                    <li onMouseOver={()=> this.mouseOver("hoveringCompany")}
-                        onMouseOut={()=> this.mouseOut("hoveringCompany")}
-                        style={styles.container}
-                    >
-                        {hoveringCompany === true && <div style={styles.tooltip}>User's Company</div>}
-                        <FaBriefcase color='#795548' size={22} />
-                        {profile.company}
+                    <li>
+                        <Tooltip text="User's Company">
+                            <FaBriefcase color='#795548' size={22} />
+                            {profile.company}
+                        </Tooltip>
                     </li>
                 )}
                 <li>
