33b4b1ccd6a8f876ed70863a450179cabf73bc99
Holmes, Morgan <mholmes@microstrategy.com>
2019-12-04 14:37:33 -0500

    add light dark theme using react context

--- app/index.js
+++ app/index.js
@@ -3,14 +3,33 @@ import ReactDOM from "react-dom"
 import './index.css'
 import Popular from './components/Popular';
 import Battle from './components/Battle'
+import {ThemeProvider} from "./contexts/Theme"
+import Nav from "./components/Nav"
 
 //component, state, lifecycle, ui
 class App extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			theme: "light",
+			toggleTheme: () => (
+				this.setState(({theme})=> ({
+					theme: theme === "dark" ? "light" : "dark"
+				}))
+			)
+		}
+	}
 	render() {
 		return (
-			<div> 
-				<Battle/>
-			</div>
+			<ThemeProvider value={this.state}>
+				<div className={this.state.theme}>
+					<div className='container'> 
+						<Nav />
+						<Battle />
+					</div>
+				</div>
+			</ThemeProvider>
 
 		)
 		
