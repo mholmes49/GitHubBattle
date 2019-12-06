a3df8d2c8b5b50b5c91fdd46e561b75df4beb743
Holmes, Morgan <mholmes@microstrategy.com>
2019-12-06 11:41:26 -0500

    add nav bar links to toggle modes with Router

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,37 @@
 import React from "react"
 import {ThemeConsumer} from "../contexts/Theme"
+import {NavLink} from 'react-router-dom'
+
+const activeStyle = {
+        color: 'rgb(187, 46, 31)'
+    }
 
 export default function Nav() {
     return (
         <ThemeConsumer>
             {({theme, toggleTheme}) =>(
                 <nav className='row space-between'>
+                    <ul className="row nav">
+                        <li>
+                            <NavLink 
+                                to="/"
+                                style={activeStyle}
+                                className="nav-link"
+                            >
+                                Popular
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink 
+                                to="/battle"
+                                style={activeStyle}
+                                className="nav-link"
+                            >
+                                Battle
+                            </NavLink>
+                        </li>
+                    
+                    </ul>
                     <button
                         style={{fontSize: 30}}
                         className='btn-clear'
